121a0a8bf7aecea60fa4a7c37b4a48144981609e
mahirfr <ilijazi.mahir@gmail.com>
2024-09-05 19:42:48 +0200

    Stack implemented

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,43 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+}
+
 export default class Stack<T> {
     public length: number;
 
-    
+    private head?: Node<T>;    
 
     constructor() {
+        this.length = 0;
     }
 
     push(item: T): void {
+        const newNode: Node<T> = {value: item};
+        if (this.length === 0) {
+            this.head = newNode;
+            this.length++;
+            return; 
+        }
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+    }
 
-}
     pop(): T | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
 
-}
+        const curr = this.head;
+        this.head = this.head?.next;
+        curr!.next = undefined;
+        this.length--;
+
+        return curr?.value;
+    }
+    
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
